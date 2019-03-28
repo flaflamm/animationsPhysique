a71496d2932bc032dc2755d78f63db83f46a46a4
mjuneau <micjuneau@gmail.com>
2019-03-28 13:28:57 -0400

    movement fonctionnel de base entre positions initiales et finales
    Menées par un slider

--- docs/utils/includes/charge.js
+++ docs/utils/includes/charge.js
@@ -2,15 +2,32 @@
 
 //Classe pour générer des symboles de charge, version +, - ou 0.
 
-SVG.extend(SVG.Circle, {
-  charge: function({signe='plus'}={signe:'plus'}) {
-  	if(signe==='plus') {
-      //TODO trouver comment ajouter les signes avec des lines
-      alert('on veut une positive icitte');
-    } else if(signe==='moins'){
-      alert('on veut une négative icitte');
-    }
 
-    return this;
-  }
+SVG.Charge = SVG.invent({
+  create: 'g',
+  inherit: SVG.G,
+  extend: {
+    constructorMethod: function({radius=50, polarite=1}={radius:50, polarite:1}) {
+      this.circle(radius).fill('#000077');
+
+      var textePolarite = 'o';
+      switch(polarite){
+        case 1:
+        textePolarite = '+';
+          break;
+        case -1:
+        textePolarite = '-';
+          break;
+      }
+      this.symbolePolarite = this.text(textePolarite).attr({x:25, y:-30});
+      this.symbolePolarite.font({anchor: 'middle', size: 60, family: 'Helvetica', fill: '#ffffff'});
+
+           return this;
+         }
+      },
+    construct: {
+        Charge: function({radius=50, polarite=1}={radius:50, polarite:1}) {
+         return this.put(new SVG.Charge).constructorMethod({radius:radius, polarite:polarite});
+       }
+    }
 });
